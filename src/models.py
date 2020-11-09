c71fecf94ba04bd93f4f346f789a862337b276df
alexcastillla <alexander.zcas@gmail.com>
2020-11-09 14:43:41 +0000

    Exercise done, updated models.py file according to the example.

--- src/models.py
+++ src/models.py
@@ -8,26 +8,73 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    userName = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    def serialize(self):
+        return {
+            "id": self.id,
+            "userName": self.userName,
+            "email": self.email,
+        }
+
+class Follower(Base):
+    __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_from_id = Column(Integer, ForeignKey('user.id'))
 
-    def to_dict(self):
-        return {}
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "user_from_id": self.user_from_id
+        }
+        
+class Comments(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    comment_text = Column(String(250), nullable=False)
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "author_id": self.user_id,
+            "post_id": self.post_id,
+            "comment_text": self.text,
+        }
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+        }
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    url = Column(String(250), nullable=False)
+    type_media = Column(Integer, nullable=False)
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "url": self.url,
+            "type_media": self.type_media,
+        }
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
